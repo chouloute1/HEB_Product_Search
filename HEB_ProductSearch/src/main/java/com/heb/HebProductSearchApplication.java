44fe4b49c65c39cea8d5480f49af2f9fdcb0b19e
Stanley Chouloute <chouloute1@gmail.com>
2018-05-10 20:06:36 -0500

    update

--- HEB_ProductSearch/src/main/java/com/heb/HebProductSearchApplication.java
+++ HEB_ProductSearch/src/main/java/com/heb/HebProductSearchApplication.java
@@ -1,17 +1,13 @@
 package com.heb;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.heb.dao.ProductRepositoryIntf;
 
 @SpringBootApplication
 public class HebProductSearchApplication implements CommandLineRunner {
 
-	@Autowired
-	private ProductRepositoryIntf productRepository;
 
 	
 	public static void main(String[] args) {
--- HEB_ProductSearch/src/main/java/com/heb/beans/Product.java
+++ HEB_ProductSearch/src/main/java/com/heb/beans/Product.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Product {
@@ -22,141 +22,83 @@ public class Product {
 	private String unit;
 	private int xFor;
 	private double cost;
-	
-	
-	
-	
+
 	public Product() {
 	}
-
-
-
 
 	public long getId() {
 		return id;
 	}
 
-
-
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
-
-
 
 	public String getDescription() {
 		return description;
 	}
 
-
-
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
-
-
-
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	public Date getLastSold() {
 		return lastSold;
 	}
-
-
-
 
 	public void setLastSold(Date lastSold) {
 		this.lastSold = lastSold;
 	}
 
-
-
-
 	public String getShelfLife() {
 		return shelfLife;
 	}
-
-
-
 
 	public void setShelfLife(String shelfLife) {
 		this.shelfLife = shelfLife;
 	}
 
-
-
-
 	public String getDepartment() {
 		return department;
 	}
-
-
-
 
 	public void setDepartment(String department) {
 		this.department = department;
 	}
 
-
-
-
 	public double getPrice() {
 		return price;
 	}
-
-
-
 
 	public void setPrice(double price) {
 		this.price = price;
 	}
 
-
-
-
 	public String getUnit() {
 		return unit;
 	}
-
-
-
 
 	public void setUnit(String unit) {
 		this.unit = unit;
 	}
 
-
-
-
 	public int getxFor() {
 		return xFor;
 	}
-
-
-
 
 	public void setxFor(int xFor) {
 		this.xFor = xFor;
 	}
 
-
-
-
+	
 	public double getCost() {
 		return cost;
 	}
 
-
-
-
 	public void setCost(double cost) {
 		this.cost = cost;
 	}
-
-
-
 
 	@Override
 	public String toString() {
@@ -165,7 +107,4 @@ public class Product {
 				+ ", cost=" + cost + "]";
 	}
 
-	
-	
-	
 }
